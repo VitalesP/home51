ecaeedb547b4e75955d444cd8ca0f7bd105f4b91
Vital <vitalesperegudov@gmail.com>
2021-01-18 20:10:54 +0600

    create project

---
+++ src/main/java/com/mongodb/home51/Home51Application.java
@@ -0,0 +1,13 @@
+package com.mongodb.home51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Home51Application.class, args);
+    }
+
+}
