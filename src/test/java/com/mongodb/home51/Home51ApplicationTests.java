c913c91fcfbb0299d3b1f5259c8f0a7d3e3c71cd
Vital <vitalesperegudov@gmail.com>
2021-01-21 14:50:57 +0600

    create project 02

---
+++ src/test/java/com/mongodb/home51/Home51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.home51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
